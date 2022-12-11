8c7ba26c01011dbb96a1b9beeade4b8dee7e4760
daeb2a <daeb2a@gmail.com>
2022-12-11 18:45:22 +0900

    6.19+ Remove To Do and React Persist

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -18,6 +18,15 @@ function ToDo({ text, category, id }: IToDo) {
       ];
     });
   }
+  const removeToDo = () => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  }
   return (
     <li>
       <span>{text}</span>
@@ -36,6 +45,9 @@ function ToDo({ text, category, id }: IToDo) {
           Done
         </button>
       )}
+      <button onClick={removeToDo}>
+        Delete
+      </button>
     </li>
   );
 }
