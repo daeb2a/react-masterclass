fcae974c534eeaea7689b8753cf523139b96d1f9
daeb2a <daeb2a@gmail.com>
2022-12-20 21:54:36 +0900

    4.1 V6 BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./routes/Home";
-import Search from "./routes/Search";
-import Tv from "./routes/Tv";
+import Router from "./Router";
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path={["/", "/movies/:movieId"]}>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div>
+    <Router />
+  </div>;
 }
 export default App;
